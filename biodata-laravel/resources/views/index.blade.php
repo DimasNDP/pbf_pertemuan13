ec7fbd124a634bb07e48eaca4ba2080f2c3a27f2
DimasNDP <nugraha.dimas312@gmail.com>
2021-05-05 18:51:23 +0700

    initial commit

---
+++ biodata-laravel/resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="/css/styles.css">
+
+        <!-- =====BOX ICONS===== -->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+        <title>Biodata</title>
+    </head>
+    <body>
+        <!--===== HEADER =====-->
+
+
+        <main class="l-main">
+            <!--===== HOME =====-->
+            <section class="home bd-grid" id="home">
+                <div class="home__data">
+                    <h1 class="home__title">Halo, Saya <br><span class="home__title-color">Dimas Nugraha D P</span><br> Kelas TI-3F</h1>
+                </div>
+
+                <div class="home__social">
+                    <a href="https://github.com/DimasNDP" class="home__social-icon"><i class='bx bxl-github' ></i></a>
+                </div>
+
+                <div class="home__img">    
+                    <img src="img/profile.jpg" alt="">
+                </div>
+            </section>
+
+
+        </main>
+
+
+        <!--===== SCROLL REVEAL =====-->
+        <script src="https://unpkg.com/scrollreveal"></script>
+
+        <!--===== MAIN JS =====-->
+        <script src="js/main.js"></script>
+    </body>
+</html>
